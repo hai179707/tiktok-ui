d0d3aac47e2caaf2400df6c6ef2b68eaa20305d2
hai179707 <nguyenthanhhai.main@gmail.com>
2022-08-28 03:18:20 +0700

    Tối ưu request api khi search

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -8,6 +8,7 @@ import styles from './Search.module.scss';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import { ClearIcon, SearchIcon } from '~/components/Icons';
 import AccountItem from '~/components/AccountItem';
+import { useDebounce } from '~/hooks';
 
 const cx = classNames.bind(styles)
 
@@ -17,17 +18,20 @@ function Search() {
     const [searchResult, setSearchResult] = useState([])
     const [showResult, setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
+
+    const debounced = useDebounce(searchValue, 500)
+
     const inputRef = useRef()
 
     useEffect(() => {
-        if (!searchValue.trim()) {
+        if (!debounced.trim()) {
             setSearchResult([])
             return
         }
 
         setLoading(true)
 
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
             .then(res => res.json())
             .then(res => {
                 setSearchResult(res.data)
@@ -36,7 +40,7 @@ function Search() {
             .catch(() => {
                 setLoading(false)
             })
-    }, [searchValue])
+    }, [debounced])
 
     const handleClear = () => {
         setSearchValue('')
