75b20b0b9af442b0e4c8db2621dbb6716ad20e9c
hai179707 <nguyenthanhhai.main@gmail.com>
2022-08-31 17:37:41 +0700

    sửa lỗi

---
+++ src/components/Discover/DiscoverList.js
@@ -0,0 +1,23 @@
+export const DISCOVER_LIST =
+    [
+        {
+            isMusicDiscover: false,
+            textContent: 'suthatla'
+        },
+        {
+            isMusicDiscover: false,
+            textContent: 'mackedoi'
+        },
+        {
+            isMusicDiscover: false,
+            textContent: 'sansangthaydoi'
+        },
+        {
+            isMusicDiscover: true,
+            textContent: 'Yêu Đơn Phương Là Gì (MEE Remix) - Mee Media & h0n'
+        },
+        {
+            isMusicDiscover: true,
+            textContent: 'Về Nghe Mẹ Ru - NSND Bach Tuyet & Hứa Kim Tuyền & 14 Casper & Hoàng Dũng'
+        }
+    ]
